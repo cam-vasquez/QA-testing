4ea86b314f595215dc5123eacbd853eed227f1b0
cam-vasquez <00065221@uca.edu.sv>
2023-06-10 12:07:54 -0600

    :sparkles: add new features

--- PruebaDiplomado/src/main/java/org/example/App.java
+++ PruebaDiplomado/src/main/java/org/example/App.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,37 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximize");
+        chromeOptions.addArguments("--remote-allow-origins=*");
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver pDriver = new ChromeDriver(chromeOptions);
+
+        // Login
+
+        pDriver.get("https://www.saucedemo.com/");
+        pDriver.findElement(By.id("user-name")).sendKeys("standard_user");
+        pDriver.findElement(By.id("password")).sendKeys("secret_sauce");
+        pDriver.findElement(By.id("login-button")).click();
+
+        // Add to cart
+        pDriver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        pDriver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt")).click();
+
+        // Delete item from cart
+        pDriver.findElement(By.id("shopping_cart_container")).click();
+        pDriver.findElement(By.id("remove-sauce-labs-bolt-t-shirt")).click();
+
+        //CheckOut
+        pDriver.findElement(By.id("checkout")).click();
+        pDriver.findElement(By.id("first-name")).sendKeys("Fernanda");
+        pDriver.findElement(By.id("last-name")).sendKeys("Serrano");
+        pDriver.findElement(By.id("postal-code")).sendKeys("001101");
+        pDriver.findElement(By.id("continue")).click();
+        pDriver.findElement(By.id("finish")).click();
+
+
+
     }
 }
